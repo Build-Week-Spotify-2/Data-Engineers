c63bb752a04a6b670d7595a22969a59609c0557e
dgansen <dtgansen@gmail.com>
2020-06-23 13:09:48 -0500

    provide Hadi's json to /predictions and it successfully receives

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 import tekore as tk
 from dotenv import load_dotenv
 import os
+import pprint
 
 app=Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
@@ -19,13 +20,38 @@ spotify = tk.Spotify(access_token)
 def root():
     return "You don't want to look at this page"
 
-@app.route('/request')
-def request():
+@app.route('/songs')
+def songs():
     tracks, = spotify.search('5 best songs',types=('track',), limit=5)
     result = ''
     for track in tracks.items:
         result = result + track.name + '\n'#' by ' + track.artists.name + 
     return result
+
+@app.route('/prediction', methods=['POST'])
+def predict():
+    '''
+    Receives input data, gets track features feeds it to model, converts to .json object and returns it
+    '''
+    # Receive JSON of song id's
+    song_inp_json = request.json
+    song_inp = song_inp_json['recommended_song_id_list']
+    #print(song_inp)
+
+    # Print song titles based on track id's
+    features = []
+    analyses = []
+    for song_id in song_inp:
+        features.append(spotify.track_audio_features(song_id))
+        analyses.append(spotify.track_audio_analysis(song_id))
+    
+    #print song info received
+    print(str(features))
+    print('\n--------\n')
+    print(str(analyses))
+
+    return 'Received!'
+
 
 if __name__=='__main__':
     app.run(debug=True)
