28ef9f1e4ce4063fada942718f37acd8e7eb6389
Jacob Desselles <jacob.desselles@gmail.com>
2020-06-24 17:18:09 -0500

    Debugged validator!

--- app.py
+++ app.py
@@ -16,10 +16,12 @@ cred = tk.Credentials(CLIENT_ID,CLIENT_SECRET)
 access_token = cred.request_client_token()
 spotify = tk.Spotify(access_token)
 def validate_post_data(data_object):
-    if ('\n' in data_object or "recommended_song_id_list" in data_object):
+    if ('Results_gathered' in data_object or "recommended_song_id_list" in data_object):
         return True
     else:
         return False
+#This function will be to validate everything is functioning properly
+#and will also specify that a user's inputs are wrong.
 @app.route('/')
 def root():
     return "Try /songs or /prediction instead." 
@@ -27,30 +29,25 @@ def root():
 
 @app.route('/songs')
 def songs():
-    tracks = spotify.search('5 best songs',types=('track',), limit=5)
+    tracks, = spotify.search('5 best songs',types=('track',), limit=5)
     result = ''
+    for track in tracks.items:
+        result = result + track.name  + '\n'#' by ' + track.artists.name + 
+    return result
     if validate_post_data(data_object=result):
-        for track in tracks.items:
-            result = result + track.name + '\n'#' by ' + track.artists.name + 
-        return result
+        correct = {'Results_gathered':result}
     else:
         return "Not a valid input!" #Right now, will only return track names with no other information
-#Output: If You're Happy And You Know It I'm A Little Teapot There Was An Old Lady Who Swallowed A Fly The Teddy Bears Picnic Hokey Pokey
-#Error: 503
-#503 means that there is a malfunction in PHP not related to Apache or LiteSpeed.
 @app.route('/prediction', methods=['POST'])
 def predict():
     '''
     Receives input data, gets track features/analysis (feeds it to model), converts to .json object and returns it
     '''
     # Receive JSON of song id's
-    if validate_post_data(data_object=song_inp_json):
-        song_inp_json = request.json
-        song_inp = song_inp_json['song_id_list']
-        song_count = song_inp_json['recommendation_count']
-        print(song_inp)
-    else:
-        return "Please check fields and try again"
+    song_inp_json = request.json
+    song_inp = song_inp_json['song_id_list']
+    song_count = song_inp_json['recommendation_count']
+    print(song_inp)
     # Print song titles based on track id's
     features = []
     analyses = []
@@ -64,19 +61,23 @@ def predict():
     # print(str(analyses))
 
     # pretend we have song recommendations
-    song_out = ["7FGq80cy8juXBCD2nrqdWU",
+    if validate_post_data(data_object=song_inp_json):
+        song_out = ["7FGq80cy8juXBCD2nrqdWU",
                 "20hsdn8oITBsuWNLhzr5eh",
                 "7fPuWrlpwDcHm5aHCH5D9t",
                 "2BOqDYLOJBiMOXShCV1neZ",
                 "67O8CWXxPsfz8orZVGMQwf"
-    ]
+        ]
+    else:
+        return "Please check fields and try again"
     # Get song features
     for song_id in song_out:
         features.append(spotify.track_audio_features(song_id))# Will display 5 songs
     
-                
-    song_out_json = {"recommended_song_id_list": song_out}
-
+    if validate_post_data(data_object=song_out):
+        song_out_json = {"recommended_song_id_list": song_out}
+    else:
+        return "Please check fields and try again"
     return str(features)#song_out_json
 #Heroku displays "METHOD NOT ALLOWED" currently for /prediction
 if __name__=='__main__':
---
+++ testing_pred.py
@@ -0,0 +1,10 @@
+from app import predict
+import unittest
+from dotenv import load_dotenv
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+import tekore as tk
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+import tekore as tk
+import app
